6cc1f470267f00716d6e2a5c1d4530b0cbf55a93
Leandro Sturniolo <ivansturni@gmail.com>
2020-05-09 18:33:06 -0300

    Commit inicial

---
+++ src/labtp8/Labtp8.java
@@ -0,0 +1,18 @@
+/*
+ */
+package labtp8;
+
+/**
+ *
+ * @author Leandro Sturniolo
+ */
+public class Labtp8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
