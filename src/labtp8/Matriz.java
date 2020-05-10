17335a6523b1695f122047738ebcb8c520c3d91f
Leandro Sturniolo <ivansturni@gmail.com>
2020-05-09 21:52:46 -0300

    Commit final

--- src/labtp8/Matriz.java
+++ src/labtp8/Matriz.java
@@ -9,15 +9,6 @@ import java.util.ArrayList;
 public class Matriz {
     
     private int[][] multi;
-    /*
-    No me dejó crear el arreglo de esta forma
-    private int[][] multi = new int[4][];
-    multi[0] = new int[4];
-    multi[1] = new int[3];
-    multi[2] = new int[5];
-    multi[3] = new int[2];
-    
-    */
 
     public Matriz() {
     }
@@ -32,17 +23,26 @@ public class Matriz {
         return ret;
     }
     
+    public String mostrarLista(ArrayList<Integer> arr) {
+        String concat = "";
+        for (Integer i : arr) {
+            concat += i + " "; 
+        }
+        return concat;
+    }
+    
     public void armar() {
         this.multi = new int[][]{
-        { 3, 6, 9, 12 },
+        { 3, 6, 9, 12},
         { 15, 18, 21},
         { 24, 27, 30, 33, 36},
         { 39, 42}
         };
         
-        System.out.println("\nFila 1: " + this.paresEnFila(multi[0]));
-        System.out.println("\nFila 2: " + this.paresEnFila(multi[1]));
-        System.out.println("\nFila 3: " + this.paresEnFila(multi[2]));
-        System.out.println("\nFila 4: " + this.paresEnFila(multi[3]));
+        System.out.println("Números pares dentro del arreglo bidimensional:");
+        
+        for (int i = 0; i < multi.length ; i++) {
+            System.out.println(" Fila " + (i+1) + "→ " + this.mostrarLista(this.paresEnFila(multi[i])));
+        }
     }
 }
